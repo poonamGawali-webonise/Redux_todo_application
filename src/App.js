417625f8876db8570dd41477b1881f9297b16a75
poonamGawali-webonise <poonam.gawali@weboniselab.com>
2020-03-05 13:31:13 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import AddTodo from './containers/AddTodo';
+import VisibleTodoList from './containers/VisibleTodoList';
+import Footer from './containers/Footer';
+
+function App() {
+  return (
+    <div className="App">
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
